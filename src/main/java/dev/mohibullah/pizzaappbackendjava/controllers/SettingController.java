3a40d96a985278a6223a43dfa5bf9d6354bd98a8
S.M. Mohibullah Bukhari <mohibullahbukhari98@gmail.com>
2023-09-28 16:37:59 +0500

    Fixed bugs

--- src/main/java/dev/mohibullah/pizzaappbackendjava/controllers/SettingController.java
+++ src/main/java/dev/mohibullah/pizzaappbackendjava/controllers/SettingController.java
@@ -1,6 +1,6 @@
 package dev.mohibullah.pizzaappbackendjava.controllers;
 
-import dev.mohibullah.pizzaappbackendjava.dtos.request.ProductRequestDTO;
+import dev.mohibullah.pizzaappbackendjava.dtos.request.SettingRequestDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,28 +14,31 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Objects;
 
 public class SettingController {
     @PutMapping("updateSettings/{id}")
-    public ResponseEntity createCategory(@PathVariable("id") int settingId, @RequestParam(value = "setting", defaultValue = "restaurant_info") int pageNo, @Valid @ModelAttribute ProductRequestDTO productRequestDTO) throws IOException {
+    public ResponseEntity createCategory(@PathVariable("id") int settingId, @RequestParam(value = "setting", defaultValue = "restaurant_info") int pageNo, @Valid @ModelAttribute SettingRequestDTO settingRequestDTO) throws IOException {
 //        return new ResponseEntity<>(controllerService.createCategory(CategoryRequestDTO), HttpStatus.CREATED);
 
-        MultipartFile LOGO = productRequestDTO.getProductImage();
-        MultipartFile splashScreen = productRequestDTO.getProductImage();
+        MultipartFile LOGO = settingRequestDTO.getRestaurant_LOGO();
+        MultipartFile splashScreen = settingRequestDTO.getRestaurant_SplashImage();
 
         // Validate image file type
-        if (!Objects.requireNonNull(productImage.getContentType()).startsWith("image/webp")) {
-            return ResponseEntity.badRequest().body("File type must be webp!");
+//        if (!Objects.requireNonNull(productImage.getContentType()).startsWith("image/webp")) {
+//            return ResponseEntity.badRequest().body("File type must be webp!");
+//        }
+
+//        System.out.println(productRequestDTO.getName());
+
+
+
+        File LOGOFile = new File("src/main/resources/static/LOGOs/",  "LOGO.webp");
+        try (OutputStream os = new FileOutputStream(LOGOFile)) {
+            os.write(LOGO.getBytes());
         }
-
-        System.out.println(productRequestDTO.getName());
-
-
-
-        File file = new File("src/main/resources/static/ProductImages/", productRequestDTO.getName() +".webp");
-        try (OutputStream os = new FileOutputStream(file)) {
-            os.write(productImage.getBytes());
+        File SplashScreenFile = new File("src/main/resources/static/ProductImages/", "SplashScreen.webp");
+        try (OutputStream os = new FileOutputStream(SplashScreenFile)) {
+            os.write(splashScreen.getBytes());
         }
 
 
--- src/main/java/dev/mohibullah/pizzaappbackendjava/dtos/request/customValidation/annotations/CustomImage.java
+++ src/main/java/dev/mohibullah/pizzaappbackendjava/dtos/request/customValidation/annotations/CustomImage.java
@@ -4,10 +4,7 @@ import dev.mohibullah.pizzaappbackendjava.dtos.request.customValidation.validato
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.*;
 
 @Target({ ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
